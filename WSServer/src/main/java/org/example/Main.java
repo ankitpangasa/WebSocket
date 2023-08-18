ef9482c833006cf513de8dd733fdea9e998afba3
ankitpangasa <78662228+ankitpangasa@users.noreply.github.com>
2023-08-19 01:02:26 +0530

    Adding server side code

---
+++ WSServer/src/main/java/org/example/Main.java
@@ -0,0 +1,69 @@
+package org.example;
+
+import java.io.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+    private static final int PORT = 1521;
+    public static void main(String[] args) throws IOException {
+        final int PORT = 1521;
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            System.out.println("Server listening on port " + PORT);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Connected to client: " + clientSocket.getRemoteSocketAddress());
+
+                Thread sendThread = new Thread(() -> {
+                    try (
+                            PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+                            BufferedReader userInputReader = new BufferedReader(new InputStreamReader(System.in))
+                    ) {
+                        String userInput;
+                        while ((userInput = userInputReader.readLine()) != null) {
+                            writer.println(userInput);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+                sendThread.start();
+
+                Thread receiveThread = new Thread(() -> {
+                    try (
+                            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+                    ) {
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            saveMessageToFile("server.txt", line);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+                receiveThread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void saveMessageToFile(String fileName, String message) {
+        // Implement saving the message to the file
+        try {
+            FileWriter fileWriter = new FileWriter(fileName, true);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            printWriter.println(message);
+            printWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
